adda6496bcfd85360c7a91076949844aa2e70017
tiagoquin <tiago.quin@gmail.com>
2019-05-11 13:04:49 +0200

    fix: add .bind() to call this inside callback

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,5 +1,7 @@
 /**
- * @fileOverview
+ * @fileOverview Entrypoint of this node app.
+ * We create a musician with an instrument passed in argument
+ * then we set an interval to make it repeat indefinitely.
  * @author  Tiago Póvoa
  */
 
@@ -10,4 +12,5 @@ const instrument = process.argv[2];
 
 const musician = new Musician(instrument);
 
-setInterval(musician.play, 5000);
+// Here the .bind() let us call this inside the play function
+setInterval(musician.play.bind(musician), 1000);
